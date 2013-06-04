04dfd7c28119dbd0efd94a0b3c4d8cdd7aeabdef
jamesl1001 <jamesl1001@msn.com>
2013-06-04 22:31:20 +0100

    Save data to variables and display example output

--- index.php
+++ index.php
@@ -1,24 +1,36 @@
 <?php
-// $csv = file_get_contents("https://docs.google.com/spreadsheet/pub?key=0Ai1UvN4cvedHdGE4eWlfdXQ3OGRVR2R5a0lNM1FqSFE&output=csv");
-// echo "CSV file_get_contents:<br>" . $csv;
-// $contentArray = str_getcsv($csv);
-// echo "<br>============<br>";
-// var_dump($contentArray);
-
-// $csv = fopen('https://docs.google.com/spreadsheet/pub?key=0Ai1UvN4cvedHdGE4eWlfdXQ3OGRVR2R5a0lNM1FqSFE&output=csv', 'r');
-// var_dump($csv);
-// $csv2 = fgetcsv($csv);
-// var_dump($csv2);
-
-$row = 1;
-if (($handle = fopen('https://docs.google.com/spreadsheet/pub?key=0Ai1UvN4cvedHdGE4eWlfdXQ3OGRVR2R5a0lNM1FqSFE&output=csv', 'r')) !== FALSE) {
-  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    $num = count($data);
-    echo "<p> $num fields in line $row: <br /></p>\n";
+if (($handle = fopen('https://docs.google.com/spreadsheet/pub?key=0Ai1UvN4cvedHdGE4eWlfdXQ3OGRVR2R5a0lNM1FqSFE&output=csv', 'r')) !== false) {
+  while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+    var_dump($data);
     $row++;
-    for ($c=0; $c < $num; $c++) {
-        echo $data[$c] . "<br />\n";
-    }
+    ${$data[0]} = $data[1];
   }
   fclose($handle);
 }
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+	<meta name="description" content="An experimental CMS using Google Drive."/>
+	<meta name="author" content="PrecisePixels"/>
+	<title>google-drive-cms</title>
+</head>
+<body>
+	<p>^ CSV to Array data ^</p>
+
+	<hr>
+
+	<h1><?= $h1; ?></h1>
+	<p><?= $paragraph; ?></p>
+
+	<ul>
+		<li><a href="#"><?= $nav1; ?></a></li>
+		<li><a href="#"><?= $nav2; ?></a></li>
+		<li><a href="#"><?= $nav3; ?></a></li>
+	</ul>
+
+	<p><?= $text1; ?></p>
+</body>
+</html>
